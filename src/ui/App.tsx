bc090080890765de780b0d6e08190dcb4781297b
DevanshSahni <devanshsahni@gmail.com>
2026-06-17 16:02:32 +0530

    setup: tailwind

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import icon from './assets/icon.svg'
 
 function App() {
@@ -6,7 +5,7 @@ function App() {
   return (
     <div style={{ width: '100%', margin: '0px', backgroundColor: '#0f222c', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
       <img style={{ width: '150px', height: '150px' }} src={icon} alt="Icon" />
-      <h1 style={{ color: 'white', textAlign: 'center' }}>Welcome to Claudé Bar!</h1>
+      <h1 className="text-white text-3xl font-bold text-center">Welcome to Claudé Bar!</h1>
     </div>
   )
 }
